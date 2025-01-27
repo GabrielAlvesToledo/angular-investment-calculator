e9e82bbc001748f16fa7e9b6ea9962ffe6505cb8
Majin <gabrielalvestoledodev@gmail.com>
2025-01-27 08:16:51 -0300

    segundou

---
+++ src/app/investment-results/investment-results.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-investment-results",
+    standalone: true,
+    templateUrl: "./investment-results.component.html",
+    styleUrl: "./investment-results.component.css"
+})
+
+export class InvestmentResults{
+
+
+
+}
--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,5 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+
+import type {InvestmentInput} from '../investment-input.model';
 
 @Component({
     selector: 'app-user-input',
@@ -10,9 +12,21 @@ import { FormsModule } from "@angular/forms";
 })
 
 
-export class UserInputComponent{
+export class UserInputComponent{ 
+    @Output() calculate = new EventEmitter<InvestmentInput>()
+
+    enteredInitialInvestment = '0';
+    enteredAnnualInvestment = '0';
+    enteredExpectedReturn = '5';
+    enteredDuration = '10';
+
     onSubmit(){
-        console.log("submitted");
+        this.calculate.emit({
+            initialInvestment: +this.enteredInitialInvestment,
+            annualInvestment: +this.enteredAnnualInvestment,
+            expectedReturn: +this.enteredExpectedReturn,
+            duration: +this.enteredDuration
+        })
     }
 
 
